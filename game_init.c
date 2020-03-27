4b943a224380ccd97f3f8a552d3bf4004ed13882
sarah-duncan <55993775+sarah-duncan@users.noreply.github.com>
2020-03-27 12:41:22 +0000

    Add files via upload
    Files for Part 1

---
+++ game_init.c
@@ -0,0 +1,93 @@
+//
+// Created by Lili on 26/03/2020.
+//
+
+//
+// Created by Lili on 24/03/2020.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "game_init.h"
+
+void initialize_players(player players[PLAYERS_NUM]){
+
+    // implement here the functionality to initialize the players
+    printf("Red Player: Please enter your name:");
+    players[0].player_colour=RED;
+    scanf("%s",&players[0].name);
+    players[0].captured = 0;
+    players[0].placable = 0;
+
+    printf("\nGreen Player: Please enter your name:");
+    players[1].player_colour=RED;
+    scanf("%s",&players[0].name);
+    players[1].captured = 0;
+    players[1].placable = 0;
+}
+
+//Set Invalid Squares (where it is not possible to place stacks)
+set_invalid(square * s){
+s->type = INVALID;
+s->stack = NULL;
+s->num_pieces = 0;
+}
+
+//Set Empty Squares (with no pieces/stacks)
+set_empty(square * s){
+s->type = VALID;
+s->stack = NULL;
+s->num_pieces = 0;
+}
+
+//Set squares  with a GREEN piece
+set_green(square * s){
+s->type = VALID;
+s->stack = (piece *) malloc (sizeof(piece));
+s->stack->p_colour = GREEN;
+s->stack->next = NULL;
+s->num_pieces = 1;
+}
+
+//Set squares with a RED piece
+set_red(square * s){
+s->type = VALID;
+s->stack = (piece *) malloc (sizeof(piece));
+s->stack->p_colour = RED;
+s->stack->next = NULL;
+s->num_pieces = 1;
+}
+
+//initializes the board
+void initialize_board(square board [BOARD_SIZE][BOARD_SIZE]){
+
+    for(int i=0; i< BOARD_SIZE; i++){
+        for(int j=0; j< BOARD_SIZE; j++){
+            //invalid squares
+            if((i==0 && (j==0 || j==1 || j==6 || j==7)) ||
+               (i==1 && (j==0 || j==7)) ||
+               (i==6 && (j==0 || j==7)) ||
+               (i==7 && (j==0 || j==1 || j==6 || j==7)))
+                set_invalid(&board[i][j]);
+
+            else{
+                //squares with no pieces
+                if(i==0 || i ==7 || j==0 || j == 7)
+                    set_empty(&board[i][j]);
+                else{
+                    //squares with red pieces
+                    if((i%2 == 1 && (j < 3 || j> 4)) ||
+                       (i%2 == 0 && (j == 3 || j==4)))
+                        set_red(&board[i][j]);
+                        //green squares
+                    else set_green(&board[i][j]);
+                }
+            }
+        }
+
+
+    }
+
+
+}
+
